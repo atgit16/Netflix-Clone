4f089336896afca623422f8092fcada9875f8b77
atgit16 <at2024pl@gmail.com>
2024-07-24 08:46:44 -0400

    Row

--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -1,8 +1,8 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 const requests = {
-    fetchTreanding: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=231`,
-    fetchTopRatedMovies: `/movies/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_geners=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_geners=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_geners=27`,
